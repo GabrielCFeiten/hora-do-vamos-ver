94a62ccbb3d1d51b14a94d5dfb3fad5014a21925
GabrielCFeiten <gabrielcamposfeiten@gmail.com>
2026-03-04 20:11:15 -0300

    Add MilagreModel

---
+++ src/main/java/com/cf/milagre/models/MilagreModel.java
@@ -0,0 +1,33 @@
+package com.cf.milagre.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_milagre")
+public class MilagreModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public MilagreModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
